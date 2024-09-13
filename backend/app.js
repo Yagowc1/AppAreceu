57ce9a07229b94abe495f0ec4e0fbf614c743ab7
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-09-13 10:33:57 -0300

    feature: Adicão das rotas dos itens e usuarios

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/usuarios');
+var itensRouter = require('./routes/itens')
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/itens', itensRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
