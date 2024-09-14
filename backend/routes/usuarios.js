bf98ba9d7ce7da374882b7d280648f92cd79ce64
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-09-13 21:47:40 -0300

    feature: Adição do CRUD e das rotas dos usuários

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -3,13 +3,32 @@ var router = express.Router();
 
 const usuarioService = require('../services/usuarioService')
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/administrador', async function(req, res, next) {
+  const adm = await usuarioService.getAdministrador()
+  res.send(adm);
 });
 
-router.get('/administrador', async function(req, res, next) {
-  const usuario = await usuarioService.getAdministrador()
-  res.send(usuario);
+router.get('/aluno', async function(req, res, next) {
+  const aluno = await usuarioService.getAluno()
+  res.send(aluno);
 });
+
+router.post('/aluno', async function(req, res, next) {
+  await usuarioService.inserirAluno(req.body)
+
+  res.sendStatus(200)
+})
+
+router.put('/aluno/:matricula', async function(req, res) {
+  const aluno = await usuarioService.atualizarAluno(req.body, req.params.matricula)
+
+  res.send(aluno)
+}) 
+
+router.delete('/aluno/:matricula', async function(req, res) {
+  await usuarioService.deletarAluno(req.params.matricula)
+
+  res.send(200)
+})
 
 module.exports = router;
--- backend/services/usuarioService.js
+++ backend/services/usuarioService.js
@@ -5,4 +5,29 @@ async function getAdministrador() {
     return response[0]
 }
 
-module.exports = { getAdministrador }
+async function getAluno() {
+    const response = await db.query('SELECT * FROM aluno')
+    return response[0]
+}
+
+async function inserirAluno(aluno) {
+    const sql = 'INSERT INTO aluno (matricula, nome, email, senha) VALUES (?, ?, ?, ?)'
+    const valores = [aluno.matricula, aluno.nome, aluno.email, aluno.senha]
+    const response = await db.query(sql, valores)
+    return response[0]
+}
+
+async function atualizarAluno(aluno, matricula) {
+    const sql = 'UPDATE aluno SET nome = ?, email = ?, senha = ? WHERE matricula LIKE ?'
+    const valores = [aluno.nome, aluno.email, aluno.senha, matricula]
+    const response = await db.query(sql, valores)
+    return response[0]
+}
+
+async function deletarAluno(matricula) {
+    const sql = 'DELETE FROM aluno WHERE matricula LIKE ?'
+    const response = await db.query(sql, matricula)
+    return response[0]
+}
+
+module.exports = { getAdministrador, getAluno, inserirAluno, atualizarAluno, deletarAluno }
