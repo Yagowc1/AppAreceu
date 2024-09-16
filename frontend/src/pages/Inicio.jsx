7ae0ef1c9406c0a6c0c5212a10ddfd303d7b0b4b
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-09-16 12:24:49 -0300

    feature: Adição de cadastro de aluno e atualização de senha

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,43 +1,51 @@
-import { useState, useContext, useEffect,  } from 'react'
+import { useState, useContext, useEffect  } from 'react'
 import { SuapContext } from '../context/SuapContext'
 import { UsuarioContext } from '../context/UsuarioContext'
 
 function Inicio() {
-    const {suap} = useContext(SuapContext)
-    const {usuario, setUsuario} = useContext(UsuarioContext)
+  const {suap} = useContext(SuapContext)
+  const {usuario, setUsuario} = useContext(UsuarioContext)
 
-    async function criarUsuario(e){
-      e.preventDefault()
-
-      let senhaValida = VerificaSenhas(senha.value, senhaConfirmada.value)
-
-      if (senhaValida) {
-        const dados = await suap.getResource()
-        console.log(usuario)
-
-        setUsuario({
-            nome: dados.nome,
-            matricula: dados.identificacao,
-            email: dados.email_secundario,
-            senha: senha.value,
-            adm: 0
-        })
-
-        await fetch('http://localhost:3000/usuarios/aluno', {
-          method:  'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            'nome': dados.nome,
-            'matricula': dados.identificacao,
-            'email': dados.email_secundario,
-            'senha': senha.value
-          })
-        }).then(dados => dados.json())
-      } else {
-        alert('As senhas não são iguais!')
-      }
-      
+  function persistirUsuarioTemp(usuario) {
+    console.log('opa')
+    localStorage.setItem('email', usuario.email)
+    localStorage.setItem('senha', usuario.senha)
+    localStorage.setItem('adm', usuario.adm)
+    
+    if (usuario.matricula) {
+      localStorage.setItem('matricula', usuario.matricula)
+      localStorage.setItem('nome', usuario.nome)
     }
+  }
+
+  async function criarAluno(){
+    await fetch('http://localhost:3000/usuarios/aluno', {
+      method:  'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'nome': usuario.nome,
+        'matricula': usuario.matricula,
+        'email': usuario.email,
+        'senha': usuario.senha
+      })
+    })
+
+    persistirUsuarioTemp(usuario)
+  }
+
+  async function atualizarSenha() {
+    await fetch(`http://localhost:3000/usuarios/aluno/${usuario.matricula}`, {
+      method:  'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'nome': usuario.nome,
+        'email': usuario.email,
+        'senha': usuario.senha
+      })
+    })
+
+    persistirUsuarioTemp(usuario)
+  }
 
   function VerificaSenhas(senha, confirmacaoSenha) {
     if (senha == confirmacaoSenha) {
@@ -46,6 +54,48 @@ function Inicio() {
       return false
     }
   }
+
+  async function verificaUsuarioExiste() {
+    console.log(usuario)
+
+    const alunoExiste = await fetch(`http://localhost:3000/usuarios/aluno/${usuario.matricula}`)
+    .then(dados => dados.json())
+    
+    console.log(alunoExiste)
+
+    if (alunoExiste.length > 0) {
+      atualizarSenha()
+    } else {
+      criarAluno()
+    }
+  }
+
+  async function pegarDados(e) {
+    e.preventDefault()
+
+    let senhaValida = VerificaSenhas(senha.value, senhaConfirmada.value)
+
+    if (senhaValida) {
+      const dados = await suap.getResource()
+      console.log(dados.nome)
+
+      setUsuario({
+        nome: dados.nome,
+        matricula: dados.identificacao,
+        email: dados.email_secundario,
+        senha: senha.value,
+        adm: 0
+      })
+    } else {
+      alert('As senhas não são iguais!')
+    }
+}
+
+  useEffect(() => {
+    if (usuario.matricula && suap.isAuthenticated()) {
+      verificaUsuarioExiste()
+    }
+  }, [usuario])
 
   return (
     <>
@@ -57,7 +107,7 @@ function Inicio() {
         </div> 
       :
       <div className="wrapper">
-        <form onSubmit={criarUsuario} method='post'>
+        <form onSubmit={pegarDados} method='post'>
             <h1 className="titulo">Digite uma nova senha</h1>
             <div className="input-box">
                 <input type="password" id='senha' placeholder="Senha" required />
